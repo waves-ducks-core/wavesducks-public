7c20f0646f71836f048c4c2353f7a99d619ccbc8
DNagtegals <dnag@blackturtle.eu>
2022-08-30 13:15:05 +0200

    collective farms automated testing WIP

--- test/cf-full-automated-tests.js
+++ test/cf-full-automated-tests.js
@@ -2,23 +2,34 @@ const DappUtils = require("./utils/dapp.utils");
 const dappUtils = new DappUtils();
 const accountUtils = require("./utils/account.utils");
 
-describe("ducks - setup smart contracts", () => {
+// account 'oracle' used in 'issueTestAsset' if defined
+const oracle = 'oracle';
+// account names used for creating Dapps
+const collectiveFarm = 'collective-farm';
+const collectiveFarmStaking = 'cf-staking';
+
+describe("ducks - setup smart contracts - good flow", () => {
   before(async function () {
+    // TODO: check why this timeout is needed
     this.timeout(0);
+
     await accountUtils.defineAccounts({
-      "collective-farms": 0.1,
-      "cf-staking": 0.1, //= f_{{CF}}_stake_address
-      masterSeed: 0.1, // example = https://wavesexplorer.com/addresses/3PDktsxDVEcoobpEBJcausnKo4enhcWUDEF?search=3PDktsxDVEcoobpEBJcausnKo4enhcWUDEF
+      oracle: 1,
+      collectiveFarm: 0.1,
+      collectiveFarmStaking: 0.1, //= f_{{CF}}_stake_address
+      // TODO: check if master seed is needed, don't think so according to docs?
+      // masterSeed: 0.1, // example = https://wavesexplorer.com/addresses/3PDktsxDVEcoobpEBJcausnKo4enhcWUDEF?search=3PDktsxDVEcoobpEBJcausnKo4enhcWUDEF
     });
 
     await dappUtils.issueTestAsset();
-    await dappUtils.setupDapp("collective-farms");
-    await dappUtils.setupDapp("cf-staking");
+    await dappUtils.setupDapp(collectiveFarm);
+    await dappUtils.setupDapp(collectiveFarmStaking);
 
-    //TODO: compose correct data to send to masterSeed (preferable with helper function)
+    //TODO: compose correct data to send to masterSeed (preferably with helper function)
   });
-  describe("Try init CF", () => {
-    it("initCollectiveFarm", async function () {
+
+  describe("Try to initiate collective farm", () => {
+    it("should run initCollectiveFarm", async function () {
       const invoke = invokeScript(
         {
           version: 1,
@@ -26,6 +37,14 @@ describe("ducks - setup smart contracts", () => {
           call: {
             function: "initCollectiveFarm",
             args: [
+              // name: String, minimumThreshold: Int, migration: Boolean, totalFarmingReward: Int,totalLiquidity: Int, totalFarmToken: Int
+              // { type: "string", value: "collective-farm-test" },
+              // { type: "integer", value: 0 },
+              // { type: "boolean", value: false },
+              // { type: "integer", value: 0 },
+              // { type: "integer", value: 0 },
+              // { type: "integer", value: 0 }
+
               { type: "string", value: "arguments" },
               { type: "integer", value: 250 },
             ],
