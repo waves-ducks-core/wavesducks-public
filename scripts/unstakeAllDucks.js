f48c919eab1de7d663d123b22462ec1528c07ab8
MrTurtle <support@turtlenetwork.eu>
2022-09-27 14:49:56 +0200

    add liquidation stuff

---
+++ scripts/unstakeAllDucks.js
@@ -0,0 +1,60 @@
+const axios = require("axios");
+const cfAddress = "";
+const senderPublicKey = "";
+const masterSeed = "";
+const farmDapp = "";
+
+(async () => {
+  axios({
+    method: "get",
+    url:
+      "https://node.turtlenetwork.eu/addresses/data/" +
+      farmDapp +
+      "?matches=address_" +
+      cfAddress +
+      "_asset_.%2A_farmingPower",
+  }).then(({ data }) => {
+    list = 0;
+    stringList = "";
+    for (let i = 0; i < data.length; i++) {
+      const key = data[i].key.split("_")[3];
+      console.log(key);
+      console.log(i);
+      console.log(data.length);
+      if (data[i].value != 0) {
+        list += 1;
+
+        if (list != 1) {
+          stringList += ";";
+        }
+
+        stringList += key;
+        if (list == 5 || i == data.length - 1) {
+          console.log(stringList);
+
+          const unstake = invokeScript(
+            {
+              version: 1,
+              additionalFee: 400000,
+              senderPublicKey: senderPublicKey,
+              dApp: cfAddress,
+              call: {
+                function: "callUnstakeProxy",
+                args: [
+                  { type: "string", value: "unstakeNFT" },
+                  { type: "string", value: stringList },
+                ],
+              },
+            },
+            masterSeed
+          );
+
+          broadcast(unstake).catch((e) => console.log(JSON.stringify(e)));
+
+          stringList = "";
+          list = 0;
+        }
+      }
+    }
+  });
+})();
