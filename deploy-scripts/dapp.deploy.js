8207f9eb92c9014201dbe864d4e60b278081b5ce
MrTurtle <support@turtlenetwork.eu>
2023-03-06 17:18:17 +0100

    add script parsin

--- deploy-scripts/dapp.deploy.js
+++ deploy-scripts/dapp.deploy.js
@@ -3,7 +3,7 @@
 // wrap out script with async function to use fancy async/await syntax
 (async () => {
   // Functions, available in tests, also available here
-  const script = compile(file(process.env.FILE + ".ride"));
+  const script = compile(file(process.env.FILE.replace(/_/g, "") + ".ride"));
 
   const dappSeed = process.env.SEED; // Or use seed phrase from surfboard.config.json
   const ssTx = setScript(
