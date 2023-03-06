299fc17cd53b96aa13838746d6744b710465c081
MrTurtle <support@turtlenetwork.eu>
2023-03-06 16:06:34 +0100

    automate babyduckbreeding deploy

---
+++ deploy-scripts/babyDuckBreeding.deploy.js
@@ -0,0 +1,18 @@
+// Wallet.ride deploy script. To run execute `surfboard run path/to/script`
+
+// wrap out script with async function to use fancy async/await syntax
+(async () => {
+  // Functions, available in tests, also available here
+  const script = compile(file("babyDuckBreeding.ride"));
+
+  const dappSeed = env.BABYDUCKBREEDINGSEED; // Or use seed phrase from surfboard.config.json
+  const ssTx = setScript(
+    {
+      script,
+    },
+    dappSeed
+  );
+  await broadcast(ssTx).catch((e) => console.log(e));
+  await waitForTx(ssTx.id).catch((e) => console.log(e));
+  console.log(ssTx.id);
+})();
