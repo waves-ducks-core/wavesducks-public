c92d930e674411d6c0c748ff03af5941067c0160
MrTurtle <support@turtlenetwork.eu>
2023-03-09 10:52:39 +0100

    rename duckhouse, adapt ticketraffle deploy

--- deploy-scripts/dapp-sub.deploy.js
+++ deploy-scripts/dapp-sub.deploy.js
@@ -3,7 +3,14 @@
 // wrap out script with async function to use fancy async/await syntax
 (async () => {
   // Functions, available in tests, also available here
-  const script = compile(file(process.env.FILE.replaceAll("_", "") + ".ride"));
+  const script = compile(
+    file(
+      process.env.FILE.split("_")[1] +
+        "/" +
+        process.env.FILE.replaceAll("_", "") +
+        ".ride"
+    )
+  );
 
   const dappSeed = process.env.SEED; // Or use seed phrase from surfboard.config.json
   const ssTx = setScript(
