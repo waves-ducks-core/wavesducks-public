5f4c648e76c07c10cbf3f893cad92a99b82494ca
gabrieljolvm <biel.jose@hotmail.com>
2025-01-03 17:20:40 -0300

    add data

--- deploy-scripts/data-deploy.js
+++ deploy-scripts/data-deploy.js
@@ -4,8 +4,8 @@ const deployData = require('./dapp-deploy-functions/deployData');
 const deployTestEnv = require('./dapp-deploy-functions/deployTestEnv');
 const setDappScripts = require('./dapp-deploy-functions/setDapps');
 
-const { ORACLE_SEED, LOOT_BOXES_SEED, HUNT_SEED } = dappSeeds;
-const dataDapps = Object.entries({ ORACLE_SEED, LOOT_BOXES_SEED, HUNT_SEED })
+const { ORACLE_SEED, LOOT_BOXES_SEED } = dappSeeds;
+const dataDapps = Object.entries({  LOOT_BOXES_SEED })
 
 async function deployAllData() {
     for (const [dappKey, { seed, path, oraclePaths }] of dataDapps) {
