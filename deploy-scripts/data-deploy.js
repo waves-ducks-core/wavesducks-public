07edc315e73cefd59be00338da7901014ae20f53
gabrieljolvm <biel.jose@hotmail.com>
2025-01-15 20:56:42 -0300

    fix puzzle

--- deploy-scripts/data-deploy.js
+++ deploy-scripts/data-deploy.js
@@ -5,7 +5,7 @@ const deployTestEnv = require('./dapp-deploy-functions/deployTestEnv');
 const setDappScripts = require('./dapp-deploy-functions/setDapps');
 
 const { ORACLE_SEED, LOOT_BOXES_SEED } = dappSeeds;
-const dataDapps = Object.entries({  LOOT_BOXES_SEED })
+const dataDapps = Object.entries({ ORACLE_SEED, LOOT_BOXES_SEED })
 
 async function deployAllData() {
     for (const [dappKey, { seed, path, oraclePaths }] of dataDapps) {
